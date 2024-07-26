aebed81a6f9cd8a426db957f96a906f0b3cc9cb5
Chris Jeon <chris@typefast.co>
2024-07-26 13:13:39 -0500

    Initial commit

---
+++ lib/tinycms.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tinycms/version"
+
+module Tinycms
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tinycms/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tinycms
+  VERSION = "0.1.0"
+end
