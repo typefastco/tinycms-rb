85d47b393a357e706d5b4851922bb280b2c92bcc
Chris Jeon <chris@typefast.co>
2024-07-26 17:02:53 -0500

    Some base models

--- lib/tinycms.rb
+++ lib/tinycms.rb
@@ -2,6 +2,9 @@
 
 require_relative "tinycms/version"
 require_relative "tinycms/configuration"
+require_relative "tinycms/author"
+require_relative "tinycms/blog"
+require_relative "tinycms/post"
 
 module Tinycms
   class Error < StandardError; end
