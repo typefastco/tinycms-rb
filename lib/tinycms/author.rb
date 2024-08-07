b2fa908a59136be987ed84b2f85d8a47ed2d9032
Chris Jeon <chris@typefast.co>
2024-08-07 13:11:12 -0500

    Update models

--- lib/tinycms/author.rb
+++ lib/tinycms/author.rb
@@ -2,15 +2,16 @@
 
 module Tinycms
   class Author
-    def initialize(id:, email:, first_name:, last_name:, created_at:, updated_at:)
+    def initialize(id:, email:, first_name:, last_name:, full_name:, created_at:, updated_at:)
       @id = id
       @email = email
       @first_name = first_name
       @last_name = last_name
+      @full_name = full_name
       @created_at = created_at
       @updated_at = updated_at
     end
 
-    attr_reader :id, :email, :first_name, :last_name, :created_at, :updated_at
+    attr_reader :id, :email, :first_name, :last_name, :full_name, :created_at, :updated_at
   end
 end
--- lib/tinycms/post.rb
+++ lib/tinycms/post.rb
@@ -2,15 +2,16 @@
 
 module Tinycms
   class Post
-    def initialize(id:, title:, author:, content:, created_at:, updated_at:)
+    def initialize(id:, title:, author:, content:, content_plain_text:, created_at:, updated_at:)
       @id = id
       @title = title
       @author = author
       @content = content
+      @content_plain_text = content_plain_text
       @created_at = created_at
       @updated_at = updated_at
     end
 
-    attr_reader :id, :title, :author, :content, :created_at, :updated_at
+    attr_reader :id, :title, :author, :content, :content_plain_text, :created_at, :updated_at
   end
 end
