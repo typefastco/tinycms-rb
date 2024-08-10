47e388c27b1e1aac3228ab098f989984e9058494
Chris Jeon <chris@typefast.co>
2024-08-10 08:10:03 -0500

    Add title for author

--- lib/tinycms/author.rb
+++ lib/tinycms/author.rb
@@ -2,17 +2,18 @@
 
 module Tinycms
   class Author
-    def initialize(id:, email:, first_name:, last_name:, full_name:, avatar_url:, created_at:, updated_at:)
+    def initialize(id:, email:, first_name:, last_name:, full_name:, title:, avatar_url:, created_at:, updated_at:)
       @id = id
       @email = email
       @first_name = first_name
       @last_name = last_name
       @full_name = full_name
+      @title = title
       @avatar_url = avatar_url
       @created_at = created_at
       @updated_at = updated_at
     end
 
-    attr_reader :id, :email, :first_name, :last_name, :full_name, :avatar_url, :created_at, :updated_at
+    attr_reader :id, :email, :first_name, :last_name, :full_name, :title, :avatar_url, :created_at, :updated_at
   end
 end
